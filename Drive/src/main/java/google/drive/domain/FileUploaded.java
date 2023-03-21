b18554d04af659ea8a4d7fd9b83407c3ef1cc91b
seimlee <56667786+seimlee@users.noreply.github.com>
2023-03-21 13:09:24 +0900

    commit

---
+++ Drive/src/main/java/google/drive/domain/FileUploaded.java
@@ -0,0 +1,25 @@
+package google.drive.domain;
+
+import google.drive.domain.*;
+import google.drive.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+@Data
+@ToString
+public class FileUploaded extends AbstractEvent {
+
+    private Long id;
+    private String filetype;
+    private String filesize;
+    private String fileid;
+    private String path;
+
+    public FileUploaded(File aggregate) {
+        super(aggregate);
+    }
+
+    public FileUploaded() {
+        super();
+    }
+}
